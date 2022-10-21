a1806cc308761fcac285c3b225e9ebbc9fff8e23
Ingo Richter <ingo.richter+github@gmail.com>
2022-10-21 13:04:26 +0200

    make script more robust

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -6,34 +6,39 @@
 
 set -uo pipefail
 
-# Step 1: Find the latest tag
-LATEST_TAG=$(curl -H "Accept: application/vnd.github+json" -L https://api.github.com/repos/protesilaos/iosevka-comfy/tags -s | jq -r '.[0] | .commit.sha,.name')
+CURL_USERAGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36"
 
+# Step 1: Find the latest tag
+LATEST_TAG=$(curl -H "Accept: application/vnd.github+json" -L --user-agent "${CURL_USERAGENT}" https://api.github.com/repos/protesilaos/iosevka-comfy/tags -s | jq -r '.[0] | .commit.sha,.name')
+
+# read the two values from the jq eval into an array
 readarray -t STRARRAY<<< "${LATEST_TAG}"
 declare -p STRARRAY
 COMMIT_SHA=${STRARRAY[0]}
 LATEST_TAG=${STRARRAY[1]}
 
-if [ -z "${GITHUB_ENV}" ]
+if [ ! -v GITHUB_ENV ] || [ -z "${GITHUB_ENV}" ]
 then
-    GITHUB_ENV=github.env
+    ENV_FILE="github.env"
+    echo "" > ${ENV_FILE}
 fi
 
-echo "COMMIT_SHA=${COMMIT_SHA}" >> "${GITHUB_ENV}"
-echo "LATEST_TAG=${LATEST_TAG}" >> "${GITHUB_ENV}"
+# preservere values to create the git tag later in the workflow
+echo "COMMIT_SHA=${COMMIT_SHA}" >> "${ENV_FILE}"
+echo "LATEST_TAG=${LATEST_TAG}" >> "${ENV_FILE}"
 
-# Step 1a: Find the comment from the commit
-COMMENT=$(curl -H "Accept: application/vnd.github+json" -L https://api.github.com/repos/protesilaos/iosevka-comfy/git/commits/${COMMIT_SHA} -s | jq -r '.message')
+# Step 1a: Find the message from the commit and write it to CHANGELOG.txt
+curl -H "Accept: application/vnd.github+json" -L --user-agent "${CURL_USERAGENT}" "https://api.github.com/repos/protesilaos/iosevka-comfy/git/commits/${COMMIT_SHA}" -s | jq -r '.message' > "CHANGELOG.txt"
 
-echo "${COMMENT}" > CHANGELOG.txt
+# echo "${COMMENT}" > CHANGELOG.txt
 
 ZIPFILE_BASENAME=iosevka-comfy-${LATEST_TAG}
 IOSEVKA_ZIP_FILE=${ZIPFILE_BASENAME}.zip
 IOSEVKA_ZIP_FILE_TARGET=iosevka-comfy.zip
 
+exit 1
 # Step 2: download the latest release
-# curl -v -L --remote-name "https://github.com/protesilaos/iosevka-comfy/archive/refs/tags/${LATEST_TAG}.zip" -o "${IOSEVKA_ZIP_FILE}"
-curl -v -L --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36" "https://github.com/protesilaos/iosevka-comfy/archive/refs/tags/${LATEST_TAG}.zip" -o "${IOSEVKA_ZIP_FILE}"
+curl -L --user-agent "${CURL_USERAGENT}" "https://github.com/protesilaos/iosevka-comfy/archive/refs/tags/${LATEST_TAG}.zip" -o "${IOSEVKA_ZIP_FILE}"
 
 # Step 3: unzip to local folder that will be iosevka-comfy-${LATEST_TAG}
 unzip "${IOSEVKA_ZIP_FILE}"
@@ -63,7 +68,7 @@ cask "font-iosevka-comfy" do
   end
 
 $(for fontName in ${fontNames}; do
-    printf "\tfont \"%s\"\n" "${fontName}"
+printf "\tfont \"%s\"\n" "${fontName}"
 done)
 end
 CASKFILE
@@ -71,20 +76,3 @@ CASKFILE
 # Step: 8 remove all temp files
 rm -rf "${ZIPFILE_BASENAME}"
 rm "${IOSEVKA_ZIP_FILE}"
-# sed -i "bak" "s/.*version \".*/  version \"${LATEST_TAG}\"/g" ./Casks/font-iosevka-comfy.rb
-# sed -i "bak" "s/.*sha256.*/  sha256 \"${SHA256}\"/g" ./Casks/font-iosevka-comfy.rb
-
-# Step 8: create a new tag
-# git pull
-# git commit --allow-empty -m "Creating Branch ${LATEST_TAG}"
-# git tag ${LATEST_TAG}
-# git push
-# git push --tags origin
-# git checkout -b ${LATEST_TAG} main
-
-# git add ./Casks/font-iosevka-comfy.rb
-# git commit -m "Update Cask File for ${LATEST_TAG} release"
-# git push -u origin ${LATEST_TAG}
-
-# Step 9: upload files for release
-export LATEST_TAG COMMENT
